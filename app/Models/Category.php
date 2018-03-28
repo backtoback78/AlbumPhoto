92fc0036c21adce098d7445d4e95007e94c90c56
backtoback78 <xmambert@gmail.com>
2018-03-28 17:53:19 +0200

    Première sauvegarde

---
+++ app/Models/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Image;
+use App\Events\CategorySaving;
+
+class Category extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug',
+    ];
+    
+    /**
+     * The event map for the model.
+     *
+     * @var array
+     */
+    protected $dispatchesEvents = [
+        'saving' => CategorySaving::class,
+    ];
+
+    /**
+     * Get the images.
+     */
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+use App\Policies\ { ImagePolicy, UserPolicy };
+use App\Models\ { Image, User };
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        Image::class => ImagePolicy::class,
+        User::class => UserPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
